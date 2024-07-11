146133f13a6f4240a3cd7bb445be1a91c91c39ca
Shreyash kawale <kawalesn01@example.com>
2024-07-12 00:25:58 +0530

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000; //  chose port from here like 8080, 3001
+const url = "https://employee-crud-json-server.onrender.com/employees"
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(url);
